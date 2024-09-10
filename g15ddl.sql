88db6b2730b379c933ab223e5cb023daca578a7c
aryeeeeeeeeel <arielsumantin69@gmail.com>
2024-09-10 19:30:40 +0800

    Group 15 DDL

---
+++ g15ddl.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 10, 2024 at 01:27 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `g15ddl`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderdetails`
+--
+
+CREATE TABLE `orderdetails` (
+  `ProductID` int(11) NOT NULL,
+  `OrderID` int(11) NOT NULL,
+  `Quantity` int(11) DEFAULT NULL,
+  `UnitPrice` decimal(10,2) DEFAULT NULL,
+  `Discount` decimal(5,2) DEFAULT NULL,
+  `StatusID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(11) NOT NULL,
+  `EmployeeID` int(11) DEFAULT NULL,
+  `CustomerID` int(11) DEFAULT NULL,
+  `OrderDate` date DEFAULT NULL,
+  `ShippedDate` date DEFAULT NULL,
+  `ShipperID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ProductID` int(11) NOT NULL,
+  `ProductCode` varchar(50) DEFAULT NULL,
+  `ProductName` varchar(100) DEFAULT NULL,
+  `Description` text DEFAULT NULL,
+  `StandardCost` decimal(10,2) DEFAULT NULL,
+  `ListPrice` decimal(10,2) DEFAULT NULL,
+  `ReorderLevel` int(11) DEFAULT NULL,
+  `TargetLevel` int(11) DEFAULT NULL,
+  `QuantityPerUnit` varchar(50) DEFAULT NULL,
+  `Discontinued` tinyint(1) DEFAULT NULL,
+  `MinimumReorderQuantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD PRIMARY KEY (`ProductID`,`OrderID`),
+  ADD KEY `OrderID` (`OrderID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ProductID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD CONSTRAINT `orderdetails_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`),
+  ADD CONSTRAINT `orderdetails_ibfk_2` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`OrderID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
